5772016fb21ad719c7284373b167542f81d954f9
junseoplee <junseoplee@soongsil.ac.kr>
2024-10-02 16:38:35 +0900

    Initial commit

---
+++ src/main/java/project/ssumc7thspringboota/Ssumc7thSpringBootAApplication.java
@@ -0,0 +1,13 @@
+package project.ssumc7thspringboota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssumc7thSpringBootAApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ssumc7thSpringBootAApplication.class, args);
+  }
+
+}
---
+++ src/test/java/project/ssumc7thspringboota/Ssumc7thSpringBootAApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ssumc7thspringboota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssumc7thSpringBootAApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
