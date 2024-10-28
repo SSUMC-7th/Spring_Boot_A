2e2b3a2c210588eaac1aaf65b0b7749fb0cbbfdb
junseoplee <junseoplee@soongsil.ac.kr>
2024-10-28 18:07:10 +0900

    chore: @EnableJpaAuditing 추가 (#5)

--- src/main/java/project/ssumc7thspringboota/Ssumc7thSpringBootAApplication.java
+++ src/main/java/project/ssumc7thspringboota/Ssumc7thSpringBootAApplication.java
@@ -2,8 +2,10 @@ package project.ssumc7thspringboota;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Ssumc7thSpringBootAApplication {
 
   public static void main(String[] args) {
