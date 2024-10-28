cfd832eb5108540d4dab8393873d724f9cdd0c1c
alswjdghks <jerrymin1221@naver.com>
2024-10-28 19:27:27 +0900

    practice: 양방향 매핑 설정

--- src/main/java/umc/study/domain/FoodCategory.java
+++ src/main/java/umc/study/domain/FoodCategory.java
@@ -1,11 +1,12 @@
 package umc.study.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import umc.study.domain.common.BaseEntity;
+import umc.study.domain.mapping.MemberPrefer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,5 +21,7 @@ public class FoodCategory extends BaseEntity {
 
     private String name;
 
+    @OneToMany(mappedBy = "foodCategory",cascade = CascadeType.ALL)
+    private List<MemberPrefer> memberPreferList = new ArrayList<>();
 
 }
