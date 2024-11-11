1191bdaf275f2ac14202a0d164d38a257ba0a5f8
ceunjinn <ceunjinn@gmail.com>
2024-11-11 22:19:57 +0900

    mission

---
+++ src/main/java/umc/spring/apiPayload/code/BaseCode.java
@@ -0,0 +1,10 @@
+package umc.spring.apiPayload.code;
+
+import java.awt.desktop.UserSessionEvent;
+
+public interface BaseCode {
+
+    ReasonDTO getReason();
+
+    ReasonDTO getReasonHttpStatus();
+}
