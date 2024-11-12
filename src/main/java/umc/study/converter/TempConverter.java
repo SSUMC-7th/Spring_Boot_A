6b816346df10601eeeef7b9f777f0b0280acb38a
jerrymin1221 <jerrymin1221@naver.com>
2024-11-12 18:32:29 +0900

    [Jerry] Chapter07_API 응답 통일 & 에러 핸들러  (#23)
    * feat: Chapter 6.JPA 활용 - queryDSL 적용하기
    
    * fix: Q타입 static import 설정
    
    * feat: API 응답 통일 & 에러 핸들러

---
+++ src/main/java/umc/study/converter/TempConverter.java
@@ -0,0 +1,17 @@
+package umc.study.converter;
+
+import umc.study.web.dto.TempResponse;
+
+public class TempConverter {
+    public static TempResponse.TempTestDTO toTempTestDTO(){
+        return TempResponse.TempTestDTO.builder()
+                .testString("this is test")
+                .build();
+    }
+
+    public static TempResponse.TempExceptionDTO toTempExceptionDTO(Integer flag){
+        return TempResponse.TempExceptionDTO.builder()
+                .flag(flag)
+                .build();
+    }
+}
