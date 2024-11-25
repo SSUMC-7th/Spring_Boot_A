dee0544efec709329da0d71e3d6c88a4fbbf8c82
alswjdghks <jerrymin1221@naver.com>
2024-11-25 20:31:39 +0900

    feat: 디렉토리 구조 초기세팅

--- src/main/java/umc/study/service/memberService/MemberQueryService.java
+++ src/main/java/umc/study/service/memberService/MemberQueryService.java
@@ -1,4 +1,9 @@
 package umc.study.service.memberService;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import umc.study.domain.Review;
+
 public interface MemberQueryService {
+    Page<Review> getReviewList(Long memberId, PageRequest pageRequest);
 }
--- src/main/java/umc/study/service/memberService/MemberQueryServiceImpl.java
+++ src/main/java/umc/study/service/memberService/MemberQueryServiceImpl.java
@@ -1,4 +1,19 @@
 package umc.study.service.memberService;
 
-public class MemberQueryServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import umc.study.domain.Review;
+import umc.study.repository.reviewRepository.ReviewRepository;
+
+@Service
+@RequiredArgsConstructor
+public class MemberQueryServiceImpl implements MemberQueryService {
+    private final ReviewRepository reviewRepository;
+
+    @Override
+    public Page<Review> getReviewList(Long memberId, PageRequest pageRequest) {
+        return null;
+    }
 }
